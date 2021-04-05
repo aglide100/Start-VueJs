ce0d91ee519a2f4916165657316c1d7d2d8be7ae
aglide100 <aglide100@gmail.com>
2021-04-05 22:30:55 +0900

    add test

---
+++ stories/Test.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import MyTest from "./Test.vue";
+
+export default Vue.extend({
+  name: "test",
+
+  components: { MyTest },
+
+  props: {},
+
+  methods: {},
+});
+</script>
